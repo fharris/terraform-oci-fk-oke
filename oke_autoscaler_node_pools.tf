50025f017cd20dff3a2def8dfca3340ee3921b62
Fernando Harris Francisco <fharris.dev@gmail.com>
2025-03-07 09:48:01 +0100

    Update oke_autoscaler_node_pools.tf

--- oke_autoscaler_node_pools.tf
+++ oke_autoscaler_node_pools.tf
@@ -5,7 +5,7 @@ resource "oci_containerengine_node_pool" "fk_oke_autoscaler_node_pool" {
   compartment_id     = var.compartment_ocid
   kubernetes_version = var.k8s_version
   name               = "Autoscaler${var.pool_name}${count.index+1}"
-  node_shape         = var.node_shape
+  node_shape         = var.autoscaler_node_shape
 
   initial_node_labels {
     key   = "nodeType"
